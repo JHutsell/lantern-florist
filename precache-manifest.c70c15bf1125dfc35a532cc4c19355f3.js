e2ef46b8ea10c47b5a08a3db6f49befac46ce7bd
Justin Hutsell <hutselljustin@gmail.com>
2019-11-20 16:57:55 -0500

    Updates

---
+++ precache-manifest.c70c15bf1125dfc35a532cc4c19355f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578461794fc5708bbfbd49e7c50a5a02",
+    "url": "/lantern-florist/index.html"
+  },
+  {
+    "revision": "56aff90ca39cef657517",
+    "url": "/lantern-florist/static/css/main.0c85ce23.chunk.css"
+  },
+  {
+    "revision": "66e7a64268fe37940b44",
+    "url": "/lantern-florist/static/js/2.4dc660d7.chunk.js"
+  },
+  {
+    "revision": "56aff90ca39cef657517",
+    "url": "/lantern-florist/static/js/main.bd5cef7b.chunk.js"
+  },
+  {
+    "revision": "04770c5ea474ce9e5ac4",
+    "url": "/lantern-florist/static/js/runtime-main.8403e5aa.js"
+  }
+]);
