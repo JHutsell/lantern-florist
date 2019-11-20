c02e7dc89f7c6b554cdf8989986d397ae58901b0
Justin Hutsell <hutselljustin@gmail.com>
2019-11-20 15:00:07 -0500

    adds Switch to React Router to test florist show page

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import Home from './Home';
+import FloristDetail from './FloristDetail';
 import './App.css';
 
 function App() {
   require('dotenv').config()
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Switch>
+      <div className="App">
+        <Home />
+      </div>
+    </Switch>
   );
 }
 
