280cd4932826b6d0554ecb9861901c032f184411
Justin Hutsell <hutselljustin@gmail.com>
2019-11-20 15:41:20 -0500

    adds show page for individual florist

--- src/Components/FloristDetail.js
+++ src/Components/FloristDetail.js
@@ -1,1 +1,22 @@
 // show page for florist
+import React from 'react';
+
+export default class FloristDetail extends React.Component {
+    
+    
+    render() {
+        let floristDetail = JSON.parse(localStorage.clickedFlorist)
+        console.log(floristDetail)
+        let displayAddress = floristDetail.location.display_address.join(", ");
+        return(
+            <div>
+                <h1>{floristDetail.name}</h1>
+                <img src={floristDetail.image_url}/>
+                <h1>{displayAddress}</h1>
+                <h1>{floristDetail.display_phone}</h1>
+                <h1>Price: {floristDetail.price}</h1>
+                <h1>Rating (1-5): {floristDetail.rating}</h1>
+            </div>
+        )
+    }
+}
--- src/Components/FloristsContainer.js
+++ src/Components/FloristsContainer.js
@@ -2,12 +2,18 @@ import React from 'react';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
 export default class FloristsContainer extends React.Component {
+    
+    handleClick = (florist) => {
+        localStorage.clickedFlorist = JSON.stringify(florist)
+        this.props.history.push('/floristDetail')
+    }
+    
     render() {
         
         let allFloristsCards = this.props.searchResults.map((florist) => {
             return(
                 <MDBCol >
-                    <MDBCard style={{ width: "22rem", background:"white", border:"solid" }}>
+                    <MDBCard style={{ width: "22rem", background:"white", border:"solid" }} onClick={() => this.handleClick(florist)}>
                         <MDBCardImage className="img-fluid" src={florist.image_url ? florist.image_url : null} style={{ width: "22rem" }} waves />
                         <MDBCardBody>
                             <MDBCardTitle>{florist.name}</MDBCardTitle>
